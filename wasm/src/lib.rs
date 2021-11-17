16b2cec73f94eedd594d72a04e6329efa4486c12
Dzenan Jupic <56133904+DzenanJupic@users.noreply.github.com>
2021-11-17 16:56:38 +0100

    implemented wasm greet

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,1 +1,11 @@
+use wasm_bindgen::prelude::wasm_bindgen;
 
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
