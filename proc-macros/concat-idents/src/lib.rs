14ee8d42ee1a1de04fff8b18c8db8f9d06068e4c
Dzenan Jupic <56133904+DzenanJupic@users.noreply.github.com>
2021-11-29 15:35:33 +0100

    implemented first working version of InputParser

--- proc-macros/concat-idents/src/lib.rs
+++ proc-macros/concat-idents/src/lib.rs
@@ -2,10 +2,11 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 
-use proc_macro2::{Ident, Span};
-use syn::{Block, Token};
+use proc_macro2::Span;
+use syn::{Block, Ident, LitBool, LitChar, LitInt, LitStr, Token};
 use syn::parse::ParseStream;
 use syn::punctuated::Punctuated;
+use syn::token::Underscore;
 
 #[derive(Debug)]
 struct InputParser {
@@ -18,14 +19,8 @@ impl syn::parse::Parse for InputParser {
     fn parse(stream: ParseStream) -> syn::Result<Self> {
         let replace_ident: Ident = stream.parse()?;
         let _: Token![=] = stream.parse()?;
-        let idents: _ = todo!();
+        let IdentParser(concatenated_ident) = stream.parse()?;
         let block: Block = stream.parse()?;
-
-        let concatenated_ident = idents
-            .into_iter()
-            .map(|ident| ident.to_string())
-            .collect::<String>();
-        let concatenated_ident = Ident::new(&concatenated_ident, Span::call_site());
 
         Ok(Self { replace_ident, concatenated_ident, block })
     }
@@ -37,8 +32,62 @@ impl InputParser {
     }
 }
 
+struct IdentParser(Ident);
 
-// fn_ident = $fn, _, $struct {
+impl syn::parse::Parse for IdentParser {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let mut ident_parts = Vec::new();
+
+        while !input.peek(syn::token::Brace) {
+            let part: IdentPart = input.parse()?;
+            ident_parts.push(part);
+
+
+            if input.peek(Token![,]) {
+                let _: Token![,] = input.parse()?;
+            } else {
+                break;
+            }
+        }
+
+        let ident = ident_parts
+            .into_iter()
+            .map(|part| {
+                match part {
+                    IdentPart::Underscore(_) => "_".to_owned(),
+                    IdentPart::Ident(ident) => ident.to_string(),
+                    _ => todo!()
+                }
+            })
+            .collect::<String>();
+        Ok(Self(Ident::new(&ident, Span::call_site())))
+    }
+}
+
+#[derive(Debug)]
+enum IdentPart {
+    Underscore(Underscore),
+    Ident(Ident),
+    Int(LitInt),
+    Bool(LitBool),
+    Str(LitStr),
+    Char(LitChar),
+}
+
+impl syn::parse::Parse for IdentPart {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        if input.peek(Ident) {
+            Ok(Self::Ident(input.parse()?))
+        } else if input.peek(Token![_]) {
+            Ok(Self::Underscore(input.parse()?))
+        } else {
+            todo!();
+        }
+    }
+}
+
+
+// fn_ident = $fn, _, $struct, {
 //     #[test]
 //     fn fn_ident() {
 //         // -- snip --
